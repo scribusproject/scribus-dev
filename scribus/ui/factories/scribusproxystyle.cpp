bc1995e2e41d1b885535a13a3e338b91a46c5533
nitramr <martinreininger@gmx.net>
2025-12-13 17:56:01 +0100

    Update scribusproxystyle.cpp

--- scribus/ui/factories/scribusproxystyle.cpp
+++ scribus/ui/factories/scribusproxystyle.cpp
@@ -97,6 +97,7 @@ void ScribusProxyStyle::setBaseStyleName(const QString &styleName)
 
 void ScribusProxyStyle::setApplicationTheme(ApplicationTheme theme)
 {
+#if (QT_VERSION >= QT_VERSION_CHECK(6, 8, 0))
 	blockRefresh = true;
 
 	// For Linux exception see bugreport: https://bugreports.qt.io/browse/QTBUG-132929
@@ -127,4 +128,5 @@ void ScribusProxyStyle::setApplicationTheme(ApplicationTheme theme)
 	}
 
 	blockRefresh = false;
+#endif
 }
